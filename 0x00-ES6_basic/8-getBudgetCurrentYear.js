4af8488939ab26b477b0dbf5280525debdc176a3
ib-babs <babatundeibrahim426@gmail.com>
2024-04-23 09:42:08 +0100

    Modifyig 8-getBudgetCurrentYear.js

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -5,11 +5,9 @@ function getCurrentYear() {
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const budget = {};
-  const [incomeKey, gdpKey, capitaKey] = [
-    `income-${getCurrentYear()}`,
-    `gdp-${getCurrentYear()}`,
-    `capita-${getCurrentYear()}`,
-  ];
+  const incomeKey = `income-${getCurrentYear()}`;
+  const gdpKey = `gdp-${getCurrentYear()}`;
+  const capitaKey = `capita-${getCurrentYear()}`;
 
   budget[incomeKey] = income;
   budget[gdpKey] = gdp;
