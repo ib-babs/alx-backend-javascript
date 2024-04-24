fe4f1a1060b48a080e0405696660b9b0e5741433
ib-babs <babatundeibrahim426@gmail.com>
2024-04-24 15:20:22 +0100

    New commit

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 /* eslint-disable no-unused-vars */
-export default function handleProfileSignup() {
+function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then((val) => {
       console.log(val[0].body, val[1].firstName, val[1].lastName);
