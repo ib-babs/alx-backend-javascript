52bf1d8d2baa2b261ca39b9e1cbe048aa9e1e988
ib-babs <babatundeibrahim426@gmail.com>
2024-04-24 15:17:09 +0100

    New commit

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolved, rejected) => {
-    if (success) resolved({ status: 200, body: 'success' });
+    if (success) resolved({ status: 200, body: 'Success' });
     else rejected(Error('The fake API is not working currently'));
   });
 }
