998f279a05c9c3b716909ee4c1a1538e81ce4d1d
ib-babs <babatundeibrahim426@gmail.com>
2024-04-24 17:40:50 +0100

    Newest commit

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
-/* eslint-disable no-unused-vars */
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()]).then(
-    (val) => {
-      console.log(val[0].body, val[1].firstName, val[1].lastName);
-    },
+    (val) => console.log(`${val[0].body} ${val[1].firstName} ${val[1].lastName}`),
     () => console.log('Signup system offline'),
   );
 }
