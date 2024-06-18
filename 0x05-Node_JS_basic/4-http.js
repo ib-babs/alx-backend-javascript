0f8b2ac1a0488d0f5240efbed9702f318dba5172
ib-babs <babatundeibrahim426@gmail.com>
2024-06-18 22:40:41 +0100

    Initial commit

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,7 +3,7 @@ const http = require("http");
 const host = "127.0.0.1";
 const port = 1245;
 
-const app = http.createServer((req, res) => {
+const app = http.createServer((_, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
   res.end("Hello Holberton School!");
