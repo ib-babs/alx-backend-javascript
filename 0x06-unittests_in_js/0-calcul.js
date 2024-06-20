9e2a703771a49b83ac0cd44c901beb0ece2f5413
ib-babs <babatundeibrahim426@gmail.com>
2024-06-20 18:22:47 +0100

    Completed Task 0 - Correcting the word mispelled [Math.rounf to Math.round]

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,10 +1,3 @@
-/**
- * calculateNumber - Function that calulate sum of two numbers
- * @param {number} a: Arg 1
- * @param {number} b: Arg 2
- * @returns Sum of two numbers
- */
-
-const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
-
-module.exports = calculateNumber;
+module.exports = function calculateNumber(a, b) {
+  return Math.round(a) + Math.round(b);
+};
