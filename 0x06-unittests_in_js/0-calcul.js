a4f318a03d4e7573c43532ab05b1965228fbdadd
ib-babs <babatundeibrahim426@gmail.com>
2024-06-20 18:21:30 +0100

    Completed Task 0 - Correcting the word mispelled [Math.rounf to Math.round]

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -5,6 +5,6 @@
  * @returns Sum of two numbers
  */
 
-module.exports = function calculateNumber(a, b) {
-  return Math.round(a) + Math.round(b);
-};
+const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
+
+module.exports = calculateNumber;
